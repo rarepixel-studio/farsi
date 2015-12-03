a7f0a0b0255f28ed1d66a030493b07f9bcbbc558
Hamid Alaei V <hamid.a85@gmail.com>
2015-12-03 11:43:27 +0330

    compare against "Hesabi" leap year calculation

---
+++ tests/SallarJdatetimeTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace OpiloTest\Farsi;
+
+use DateInterval;
+use DatePeriod;
+use jDateTime;
+use Opilo\Farsi\JalaliDate;
+use PHPUnit_Framework_TestCase;
+
+class SallarJdatetimeTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function brute_force_test_against_sallar_jdatetime()
+    {
+        $firstDate = (new JalaliDate(1343, 1, 1))->toDateTime();
+        $lastDate = (new JalaliDate(1473, 12, 30))->toDateTime();
+        $interval = DateInterval::createFromDateString('1 day');
+        /** @var DatePeriod|\DateTime[] $period */
+        $period = new DatePeriod($firstDate, $interval, $lastDate);
+        foreach ($period as $date) {
+            $oJDate = JalaliDate::fromDateTime($date)->format('Y-m-d', false);
+            $sJDate = jDateTime::date('Y-m-d', $date->getTimestamp(), false, true);
+            $this->assertEquals($oJDate, $sJDate);
+        }
+    }
+
+    public function test_prev_mismatch_event()
+    {
+        $gDate = \DateTime::createFromFormat('Y-m-d', '1963-03-21');
+        $oDate = JalaliDate::fromDateTime($gDate)->format('Y-m-d', false);
+        $sDate = (jDateTime::date('Y-m-d', $gDate->getTimestamp(), false, true));
+        $this->assertEquals('1341-12-30', $oDate);
+        $this->assertEquals('1342-01-01', $sDate);
+    }
+
+    public function test_next_mismatch_event()
+    {
+        $gDate = \DateTime::createFromFormat('Y-m-d', '2095-3-20');
+        $oDate = JalaliDate::fromDateTime($gDate)->format('Y-m-d', false);
+        $sDate = (jDateTime::date('Y-m-d', $gDate->getTimestamp(), false, true));
+        $this->assertEquals('1473-12-30', $oDate);
+        $this->assertEquals('1474-01-01', $sDate);
+    }
+}
